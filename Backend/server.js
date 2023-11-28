3c504d34822baedfcd5defb2a67f2be5828a9ac9
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 09:23:32 +0100

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,21 @@
-import  express  from "express";
-import  dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit: "50mb"})) //parse json data inside the req body
-app.use(express.urlencoded({extended: true})) // parse form data inside the req body
-app.use(cookieParser())
+app.use(express.json({ limit: "50mb" })); //parse json data inside the req body
+app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Welcome Home 🏡")
-})
+  res.send("Welcome Home 🏡");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)})
