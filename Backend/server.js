826b970c0df86ae692bf02c43c83a84818ca3bdb
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 14:07:43 +0100

    update

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import userRoutes from "./routes/userRoutes"
 
 dotenv.config();
 
@@ -13,12 +14,17 @@ app.use(express.json({ limit: "50mb" })); //parse json data inside the req body
 app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
 app.use(cookieParser());
 
+app.use("/api/users", userRoutes )
+
 app.get("/", (req, res) => {
   res.send("Welcome Home 🏡");
 });
+
+
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Is Running On PORT ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Is 🏃‍♂️ On PORT ${PORT}`));
   })
   .catch((err) => console.log(err));
