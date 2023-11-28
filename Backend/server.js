661d704191723ffe322820e03ab15ab4493dff21
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 09:12:25 +0100

    y

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+import  express  from "express";
+import  dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json({limit: "50mb"})) //parse json data inside the req body
+app.use(express.urlencoded({extended: true})) // parse form data inside the req body
+app.use(cookieParser())
