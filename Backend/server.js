b678e91487f6807f29bd5a71cd28715fd724db01
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 09:15:54 +0100

    d

--- Backend/server.js
+++ Backend/server.js
@@ -13,4 +13,6 @@ app.use(express.json({limit: "50mb"})) //parse json data inside the req body
 app.use(express.urlencoded({extended: true})) // parse form data inside the req body
 app.use(cookieParser())
 
-app.get("/", (req, res))
+app.get("/", (req, res) => {
+    res.send("Welcome Home 🏡")
+})
