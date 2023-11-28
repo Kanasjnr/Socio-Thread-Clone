ae132b2662e17a0a3c80885701a9aee3a60a08a6
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 15:06:12 +0100

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,17 @@
+const User = require('../models/userModel')
+
 const signUpUser = async (req, res) => {
-   
+   try {
+        const {name, email, username, password} = req.body
+        const user = await User.findOne({$or :[{email}, {username}]})
+
+        if(user) {
+            return res.status(400).json({message: 'user already exists'})
+        }
+    
+   } catch (error) {
+    
+   }
 }
 
 module.exports = {
