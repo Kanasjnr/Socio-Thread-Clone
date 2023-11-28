689a168eaae37e7a939a2d84fd4fc0775beaa116
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-28 15:00:07 +0100

    update

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const  express = require ('express')
-const { signUpUser } = require("../controllers/userController")
+const { signUpUser } = require ("../controllers/userController")
 
 const router = express.Router()
 
