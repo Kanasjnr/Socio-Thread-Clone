372f21f3f2c9431afa110a6aa5d55e3b6cb5e0f9
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-30 11:53:25 +0100

    update

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -4,7 +4,8 @@ const protectRoute = require('../middleware/protectRoute');
 
 const router = express.Router();
 
-
+router.get("/", getPost)
 router.post("/create",protectRoute, createPost)
+
 
 module.exports = router
