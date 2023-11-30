daa23777a8706abd48b5b4ee93491db4c611b108
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-30 10:52:29 +0100

    s

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -1,4 +1,13 @@
-const createPost = async (req, res) => {};
+const createPost = async (req, res) => {
+    try {
+        const {postedBy,text,img} =req.body
+        if(!postedBy || !text) {
+            return res.status(400).json({message: "postedBy and text fields are required"})
+        }
+    } catch (error) {
+        
+    }
+};
 
 module.exports = {
   createPost,
