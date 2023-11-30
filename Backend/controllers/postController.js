79779f12a58f1bfde4a90a2bb236135335661f02
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-30 12:34:09 +0100

    update

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -59,11 +59,12 @@ const deletePost = async (req, res) => {
       return res.status(404).json({ message: "post not found" });
     }
 
-    if(post.postedBy.toString() !== req.user._id.toString()) {
-        return res.status(404).json({ message:"unathorized to delete post" });
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(404).json({ message: "unathorized to delete post" });
     }
-  
-    
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "post deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
     console.log("Error in delete post: ", error.message);
