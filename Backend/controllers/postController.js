af84b23d7e1e7aac3eee4af2eecbf4a2f5f1e634
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-30 10:54:06 +0100

    x

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -1,16 +1,16 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
 const createPost = async (req, res) => {
-    try {
-        const {postedBy,text,img} =req.body
-        if(!postedBy || !text) {
-            return res.status(400).json({message: "postedBy and text fields are required"})
-        }
-
-        const user = await User.findById(postedBy)
-    } catch (error) {
-        
+  try {
+    const { postedBy, text, img } = req.body;
+    if (!postedBy || !text) {
+      return res
+        .status(400)
+        .json({ message: "postedBy and text fields are required" });
     }
+
+    const user = await User.findById(postedBy);
+  } catch (error) {}
 };
 
 module.exports = {
