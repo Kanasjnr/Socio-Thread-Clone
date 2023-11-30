2fc63386fc3c03b199e141bdadd4bb72edcb689b
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-30 09:17:27 +0100

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,122 +1,164 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const generateTokenAndSetCookie = require("../utils/helpers/generateTokenAndSetCookies");
+const mongoose = require("mongoose");
+
+const getUserProfile = async (req, res) => {
+  // We fetch the user profile either by username or userId
+  // Query is either username or userId
+
+  const { query } = req.params;
+  try {
+    let user;
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query }).select("-updatedAt");
+    }else{
+      // query is username
+      user = await User.findOne({ username }).select("-password").select("-updatedAt");
+    }
+    if(!user)
+    return res.status(404).json({error: "User not found"})            
+    res.status(200).json(user)
+
+} catch (err) {
+res.status(500).json({error: err.message});
+console.log("Error in getUserProfile", err.message);
+}
+
+};
+
+
 
 const signUpUser = async (req, res) => {
-	try {
-		const { name, email, username, password } = req.body;
-		const user = await User.findOne({ $or: [{ email }, { username }] });
-		if (user) {
-			return res.status(400).json({ message: "User already exists" });
-		}
+  try {
+    const { name, email, username, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-		const newUser = new User({
-			name,
-			email,
-			username,
-			password: hashedPassword,
-		});
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
 
-		await newUser.save();
+    await newUser.save();
 
-		if (newUser) {
-			generateTokenAndSetCookie(newUser._id, res);
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
 
-			res.status(201).json({
-				_id: newUser._id,
-				name: newUser.name,
-				email: newUser.email,
-				username: newUser.username,
-			});
-		} else {
-			res.status(400).json({ message: "Invalid user data" });
-		}
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-		console.log("Error in sugnupUser: ", error.message);
-	}
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in sugnupUser: ", error.message);
+  }
 };
 
 const loginUser = async (req, res) => {
-	try {
-		const { username, password } = req.body;
-		const user = await User.findOne({ username: username });
-		const isPasswordCorrect = await bcrypt.compare(
-			password,
-			user?.password || ""
-		);
-		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({ error: "Invalid username or password" }); //bad request
-		}
-		
-		if (user.isFrozen) {
-			user.isFrozen = false;
-			await user.save();
-		}
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" }); //bad request
+    }
 
-        generateTokenAndSetCookie(user._id, res)
+    if (user.isFrozen) {
+      user.isFrozen = false;
+      await user.save();
+    }
 
-		res.status(200).json({
-			_id: user._id,
-			name: user.name,
-			email: user.email,
-			username: user.username,
-			bio: user.bio,
-			profilePic: user.profilePic
-		});
-	} catch (err) {
-		res.status(500).json({ message: err.message }); //Internal server error
-		console.log("Error in loginUser: ", err.message);
-	}
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message }); //Internal server error
+    console.log("Error in loginUser: ", err.message);
+  }
 };
 
 const logoutUser = (req, res) => {
-	try {
-		res.cookie("jwt", "", {maxAge: 1})
-		res.status(200).json({message: "User logged out successfully"})
-	} catch (err) {
-		res.status(500).json({ message: err.message }); //Internal server error
-		console.log("Error in logout", err.message);
-	}
-}
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message }); //Internal server error
+    console.log("Error in logout", err.message);
+  }
+};
 
 const followUnfollowUser = async (req, res) => {
-	try {
-		const {id} = req.params
-		const userToModify = await User.findById(id)
-		const currentUser = await User.findById(req.user._id)
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
 
-		if (id === req.user._id.toString()){
-			return res.status(400).json({error: 'You can not follow/unfollow yourself' })
-		}
-		if (!userToModify || !currentUser) {
-			return res.status(400).json({ error: "User not Found"})
-		}
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can not follow/unfollow yourself" });
+    }
+    if (!userToModify || !currentUser) {
+      return res.status(400).json({ error: "User not Found" });
+    }
 
-		const isFollowing = currentUser.following.includes(id)
-		if(isFollowing) {
-			//Unfollow User
-			await User.findByIdAndUpdate(id, {$pull: {followers: req.user._id}})
-			await User.findByIdAndUpdate(req.user._id, {$pull: {following: id}})
-			res.status(200).json({mesage: "User unfollowed Successfully"})
-		} else {
-			//FOLLOW USER
-			await User.findByIdAndUpdate(id, {$push: {followers: req.user._id}})
-			await User.findByIdAndUpdate(req.user._id, {$push: {following: id}})
-			res.status(200).json({mesage: "User followed Successfully"})
-		}
-	} catch (err) {
-		res.status(500).json({ message: err.message }); //Internal server error
-		console.log("Error in followUnFollowUser: ", err.message);
-	}
+    const isFollowing = currentUser.following.includes(id);
+    if (isFollowing) {
+      //Unfollow User
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ mesage: "User unfollowed Successfully" });
+    } else {
+      //FOLLOW USER
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ mesage: "User followed Successfully" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message }); //Internal server error
+    console.log("Error in followUnFollowUser: ", err.message);
+  }
+};
+
+
+const updateUser = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json({ message: err.message }); //Internal server error
+    console.log("Error in Update Usesr ", err.message);
+  }
 }
 
-
-const userProfile = () =>{
-
-}
-
-module.exports = { followUnfollowUser, signUpUser, loginUser, logoutUser, userProfile};
+module.exports = {
+  followUnfollowUser,
+  signUpUser,
+  loginUser,
+  logoutUser,
+  getUserProfile,
+  updateUser
+};
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ const {
   logoutUser,
   followUnfollowUser,
   getUserProfile,
+  updateUser
 } = require("../controllers/userController");
 const protectRoute = require("../middleware/protectRoute");
 
@@ -16,5 +17,6 @@ router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followUnfollowUser); //toggle state (follow/unfollow)
+router.post("/update/:id", protectRoute, updateUser)
 
 module.exports = router;
