7b92cc3c8e087b0a296ed5601880979ae6500a8d
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-07 10:33:25 +0100

    update

--- FrontEnd/src/components/CreatePost.jsx
+++ FrontEnd/src/components/CreatePost.jsx
@@ -78,7 +78,7 @@ const CreatePost = () => {
             {imgUrl && (
               <Flex mt={"full"} position={"relative"}>
                  <Image src={imgUrl} alt="select image"/>
-                 <CloseButton onClick={() => setImgUrl("")}/>
+                 <CloseButton onClick={() => setImgUrl("")} right={2} top={2} pos={"absolute"} bg={{"gray.800"}}/>
               </Flex>
             )}
           </ModalBody>
