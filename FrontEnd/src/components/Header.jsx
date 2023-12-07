834cb08fa4bf3ba2027c9c5cc8ab5cf272c8f3a1
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-07 14:53:57 +0100

    update

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -18,6 +18,10 @@ const Header = () => {
             </Link>
             
          )}
+
+         {!user && (
+            
+         )}
          <Image
             cursor="pointer"
             alt="logo"
