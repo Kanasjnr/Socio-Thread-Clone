102cd845fb2dc113f5527ab1d6affeb3a44978ac
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-11 09:58:39 +0100

    update

--- FrontEnd/src/components/UserHeader.jsx
+++ FrontEnd/src/components/UserHeader.jsx
@@ -16,7 +16,7 @@ const UserHeader = ({ user }) => {
   const currentUser = useRecoilValue(userAtom);
   const [following, setFollowing] = useState(
     user.followers.includes(currentUser._id)
-  );
+  ); 
   const showToast = useShowToast();
   const [updating, setUpdating] = useState(false);
 
