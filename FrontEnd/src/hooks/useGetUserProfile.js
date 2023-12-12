ef51a6b829662a9e69519634da03edea49627cdb
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 10:11:18 +0100

    z

--- FrontEnd/src/hooks/useGetUserProfile.js
+++ FrontEnd/src/hooks/useGetUserProfile.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import useShowToast from './useShowToast'
 
 const useGetUserProfile = () => {
  const [user, setUser] = useState(null)
  const [loading, setLoding] = useState(true)
  const {username} = useParams()
+ const showToast = useShowToast()
 }
 
 export default useGetUserProfile
