d9b0eab119108e7b98b1590b12603b3da2105881
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 10:02:02 +0100

    d

--- FrontEnd/src/pages/PostPage.jsx
+++ FrontEnd/src/pages/PostPage.jsx
@@ -18,7 +18,7 @@ const PostPage = () => {
   // const [liked, setLiked] = useState(false);
   const [user, setUser] = useState(null);
   useEffect(() => {
-    const getUser = async ()=>{}
+    const getUser = async () => {};
   }, []);
 
   return (
