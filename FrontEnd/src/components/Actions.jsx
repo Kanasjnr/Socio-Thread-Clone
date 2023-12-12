39a0bd09768e9552cfc5ca55f981868fa08a79bd
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 11:44:12 +0100

    update

--- FrontEnd/src/components/Actions.jsx
+++ FrontEnd/src/components/Actions.jsx
@@ -133,7 +133,6 @@ const Actions = ({ post: post_ }) => {
         >
           <title>Comment</title>
           <path
-            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
             fill="none"
             stroke="currentColor"
             strokeLinejoin="round"
