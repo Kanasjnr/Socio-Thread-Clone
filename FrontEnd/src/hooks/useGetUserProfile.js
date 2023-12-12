891d62eb84fd13f64e743b77cc2b8332eb6261be
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 10:09:43 +0100

    z

--- FrontEnd/src/hooks/useGetUserProfile.js
+++ FrontEnd/src/hooks/useGetUserProfile.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 const useGetUserProfile = () => {
  const [user, setUser] = useState(null)
+ const [loading, setLoding] = useState(null)
 }
 
 export default useGetUserProfile
