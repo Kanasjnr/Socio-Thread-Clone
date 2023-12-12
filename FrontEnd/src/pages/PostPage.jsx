945cf44a6b9fb7e1aa512be12b01e7beb43d485f
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 10:05:15 +0100

    update

--- FrontEnd/src/pages/PostPage.jsx
+++ FrontEnd/src/pages/PostPage.jsx
@@ -13,18 +13,31 @@ import { BsThreeDots } from "react-icons/bs";
 import Actions from "../components/Actions";
 import Comments from "../components/Comments";
 import { useEffect } from "react";
+import useShowToast from "../hooks/useShowToast";
 
 const PostPage = () => {
   // const [liked, setLiked] = useState(false);
   const [user, setUser] = useState(null);
+  const showToast = useShowToast()
+
   useEffect(() => {
     const getUser = async () => {
       try {
-        
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setUser(data);
       } catch (error) {
-        
+        showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
       }
-    };
+    }
+    getUser()
   }, []);
 
   return (
