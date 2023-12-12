c6414943edf5a3bf450fdc57edbc49aabb07f8cd
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-12 10:10:43 +0100

    z

--- FrontEnd/src/hooks/useGetUserProfile.js
+++ FrontEnd/src/hooks/useGetUserProfile.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const useGetUserProfile = () => {
  const [user, setUser] = useState(null)
  const [loading, setLoding] = useState(true)
+ const {username} = useParams()
 }
 
 export default useGetUserProfile
