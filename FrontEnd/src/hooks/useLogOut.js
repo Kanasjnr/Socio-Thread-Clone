7fbc9b23b6e9b55663e87387b0cb4d5ac398215b
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-12-13 09:35:51 +0100

    update

--- FrontEnd/src/hooks/useLogOut.js
+++ FrontEnd/src/hooks/useLogOut.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useRecoilState } from "recoil";
+import useShowToast from "./useShowToast";
+import userAtom from "../atoms/userAtom";
+
 
 const useLogOut = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const setUser = useRecoilState(userAtom)
+    const showToast = useShowToast()
+    
+    const logout = async () => {
+        try {
+          const res = await fetch("/api/users/logout", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+          });
+          const data = await res.json();
+    
+          if (data.error) {
+            useShowToast("Error", data.error, "error");
+            return;
+          }
+          localStorage.removeItem("user-threads");
+          setUser(null);
+        } catch (error) {
+          showToast("Error", error, "error");
+        }
+      };
+      return logout
+};
 
-export default useLogOut
+export default useLogOut;
